23fd4035a461a63db1b9043612d59fb3a4b95bef
Logan <loganvoravong@gmail.com>
2024-06-19 21:48:20 -0400

    .

--- NetInfo Explorer/Form1.Designer.cs
+++ NetInfo Explorer/Form1.Designer.cs
@@ -38,6 +38,7 @@
             ClientSize = new Size(800, 450);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
--- NetInfo Explorer/Form1.cs
+++ NetInfo Explorer/Form1.cs
@@ -1,10 +1,16 @@
 namespace NetInfo_Explorer
+    //this is a comment
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
