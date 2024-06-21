00c351ab2d7bcbaaf3dc661f3e9c0f67c105e525
Logan <loganvoravong@gmail.com>
2024-06-21 14:22:13 -0400

    added 4 buttons and made all components functional

--- NetInfo Explorer/Form1.Designer.cs
+++ NetInfo Explorer/Form1.Designer.cs
@@ -30,15 +30,18 @@
         {
             button1 = new Button();
             textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(514, 528);
-            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Location = new Point(360, 317);
             button1.Name = "button1";
-            button1.Size = new Size(126, 53);
+            button1.Size = new Size(88, 32);
             button1.TabIndex = 0;
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = true;
@@ -47,21 +50,63 @@
             // textBox1
             // 
             textBox1.BackColor = SystemColors.Window;
-            textBox1.Location = new Point(222, 349);
-            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Location = new Point(155, 209);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(711, 31);
+            textBox1.Size = new Size(499, 23);
             textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(155, 86);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 21);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(294, 84);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(428, 84);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(567, 84);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(1143, 750);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(button1);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "NetInfo Explorer";
             Load += Form1_Load;
@@ -73,5 +118,9 @@
 
         private Button button1;
         private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- NetInfo Explorer/Form1.cs
+++ NetInfo Explorer/Form1.cs
@@ -15,7 +15,28 @@ namespace NetInfo_Explorer
 
         private void button1_Click(object sender, EventArgs e)
         {
+            String text = textBox1.Text;
+            MessageBox.Show("Submit Button: " + text);
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 2 Clicked");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 3 Clicked");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 4 Clicked");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 5 Clicked");
         }
     }
 }
